5408d8d1268c71ca25a8affb2eb9ed5239624d4f
fitritrii <fitritrii4808@gmail.com>
2019-09-28 20:24:33 +0800

    Tugas3-4

---
+++ app/Http/Controllers/KategoriGaleriController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\KategoriGaleri;
+
+class KategoriGaleriController extends Controller
+{
+    
+	 public function index()
+    {
+        $kategori_galeri=KategoriGaleri::all();
+        
+        return view('kategori_galeri.index',compact('kategori_galeri'));
+    }
+}
